f1e541b9862d15de0a6dc7e38a1b08ea6b61bca7
David Rivers <dmr41.rivers@gmail.com>
2016-01-23 18:05:54 -0700

    not fully working

---
+++ controllers/board_controller.js
@@ -0,0 +1,5 @@
+// console.log("I loaded");
+// var bob = require('./chessmaster_controller.js')
+// console.log("so did I");
+// // chills.testrun()
+// bob.chicken()
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
+// var bob = require('../controllers/board_controller')
 // var chessInit = require('chessboardjs')
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
   // var board1 = ChessBoard('board1', 'start');
   res.render('index', { title: "David's first Attempt" });
 });
